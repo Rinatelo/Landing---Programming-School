b905d4378a6a120254b8cc0fb7f83c51004374ee
Rinatelo <rinat.abzalutdinov@gmail.com>
2022-07-05 23:17:01 +0300

    add project

---
+++ src/js/components/validation.js
@@ -0,0 +1,47 @@
+import { validateForms } from '../functions/validate-forms';
+
+const rules = [
+  {
+    ruleSelector: '.input-name',
+    rules: [
+      {
+        rule: 'required',
+        value: true,
+        errorMessage: 'Заполните имя!'
+      }
+    ]
+  },
+  {
+    ruleSelector: '.input-tel',
+    tel: true,
+    telError: 'Введите корректный телефон',
+    rules: [
+      {
+        rule: 'required',
+        value: true,
+        errorMessage: 'Заполните телефон!'
+      }
+    ]
+  },
+  {
+    ruleSelector: '.input-email',
+    rules: [
+      {
+        rule: 'required',
+        value: true,
+        errorMessage: 'Заполните email!'
+      },
+      {
+        rule: 'email',
+        value: true,
+        errorMessage: 'Ввидите корректный email!'
+      }
+    ]
+  },
+];
+
+const afterForm = () => {
+  console.log('Произошла отправка, тут можно писать любые действия');
+};
+
+validateForms('.order__form', rules, afterForm);
